75261b13a04cd8c025d9d0773fb6c9cc641f2f6f
Adjiri <racim-mohamed.adjiri@epitech.eu>
2025-10-24 17:40:29 +0200

    add

--- src/main/java/org/example/snackmachine/domain/snack/Machine.java
+++ src/main/java/org/example/snackmachine/domain/snack/Machine.java
@@ -1,17 +1,36 @@
 package org.example.snackmachine.domain.snack;
-
 import java.util.List;
+import java.util.Objects;
 
 public class Machine {
 
-    List<SnackPack> snacks;
+    List<Snack> snacks;
     List<SnackPack> order;
     Integer balance;
 
-    public Machine(List<SnackPack> snacks, List<SnackPack> order, Integer balance) {
+    public Machine(List<Snack> snacks, List<SnackPack> order, Integer balance) {
         this.snacks = snacks;
         this.order = order;
         this.balance = balance;
     }
 
+    void getAllProduct(){
+        for (Snack snack : snacks) {
+            System.out.println(snack.name);
+        }
+    }
+
+    String findProduct(Integer id){
+        for (Snack snack : snacks) {
+            if (Objects.equals(snack.id, id))
+                return snack.name;
+        }
+        throw new IllegalArgumentException("Le produit n'existe pas");
+    }
+
+    void getCurrentOrder(){
+        for (SnackPack snackpack : order){
+            System.out.println(findProduct(snackpack.id));
+        }
+    }
 }
