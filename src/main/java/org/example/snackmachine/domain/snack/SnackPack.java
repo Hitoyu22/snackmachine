fb7a03bacd4f294b2e1333b467dca1bd6db2f8c6
Rémy THIBAUT <remy.thibaut2005@gmail.com>
2025-10-24 16:42:04 +0200

    Add SnackPack Method

--- src/main/java/org/example/snackmachine/domain/snack/SnackPack.java
+++ src/main/java/org/example/snackmachine/domain/snack/SnackPack.java
@@ -2,12 +2,44 @@ package org.example.snackmachine.domain.snack;
 
 public class SnackPack {
 
-    Integer id;
-    Integer quantity;
+    public final Integer id;
+    public Integer quantity;
 
-    SnackPack(Integer id, Integer quantity) {
+    public SnackPack(Integer id, Integer quantity) {
         this.id = id;
-        this.quantity = quantity;
+        if (quantity <= 0){
+            throw new IllegalArgumentException("La quantité doit être supérieure à 1");
+        } else if (quantity > 20) {
+            throw new IllegalArgumentException("La quantité doit être inférieure à 20");
+        } else {
+            this.quantity = quantity;
+        }
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void increaseQuantity(Integer amount) {
+        if (amount <= 0){
+            throw new IllegalArgumentException("La quantité ajoutée doit être supérieure à 0");
+        }
+        if (quantity + amount > 20){
+            throw new IllegalArgumentException("La quantité ajoutée doit être inférieur à ce qu'il faut pour atteindre 20");
+        }
+        quantity = quantity + 1;
+    }
+
+    public void decreaseQuantity(Integer amount) {
+        if (amount <= 0){
+            throw new IllegalArgumentException("La quantité retirée doit être supérieure à 0");
+        } else if (amount > this.quantity) {
+            throw new IllegalArgumentException("Il n'est pas posssible de retiré plus que disponible dans le panier.");
+        }
     }
 
 }
