afde0a0fbc32ffeaa784821745004ec42c0345bb
Adjiri <racim-mohamed.adjiri@epitech.eu>
2025-10-24 16:47:23 +0200

    add : function to increase and decrease quantity of snack

--- src/main/java/org/example/snackmachine/domain/snack/Snack.java
+++ src/main/java/org/example/snackmachine/domain/snack/Snack.java
@@ -2,13 +2,16 @@ package org.example.snackmachine.domain.snack;
 
 public class Snack {
     Integer id;
+    Integer quantity;
     String name;
     Integer price;
     String description;
     String url;
+    private static final int MAX_STOCK = 20;
 
-    public Snack(Integer id,String name, Integer price, String description, String url) {
+    public Snack(Integer id, Integer quantity, String name, Integer price, String description, String url) {
         this.id = id;
+        this.quantity = quantity;
         this.name = name;
         this.price = price;
         this.description = description;
@@ -41,5 +44,33 @@ public class Snack {
     }
     void setDescription(){
         this.description = description;
+    }
+
+    Integer getQuantity(){
+        return quantity;
+    }
+    void setQuantity(Integer quantity){
+        this.quantity = quantity;
+    }
+
+    boolean isAvailable(int needed){
+        return quantity - needed >= 0;
+    }
+
+    void decreaseQuantity(Integer amount) {
+        if (amount <= 0){
+            throw new IllegalArgumentException("La quantité doit être positive.");
+        }
+        if (!isAvailable(amount)){
+            throw new IllegalStateException("Stock insuffisant");
+        }
+        this.quantity = this.quantity - amount;
+    }
+
+    void increaseQuantity(Integer amount) {
+        if (amount <= 0){
+            throw new IllegalArgumentException("La quantité doit être positive.");
+        }
+        this.quantity = Math.min(this.quantity + amount, MAX_STOCK);
     }
 }
