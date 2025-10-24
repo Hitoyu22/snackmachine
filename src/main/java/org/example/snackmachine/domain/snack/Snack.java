0f79bada50c9d39aaf6fc39f96a393483f57ffe5
Adjiri <racim-mohamed.adjiri@epitech.eu>
2025-10-24 16:05:42 +0200

    add: snack

--- src/main/java/org/example/snackmachine/domain/snack/Snack.java
+++ src/main/java/org/example/snackmachine/domain/snack/Snack.java
@@ -1,20 +1,45 @@
 package org.example.snackmachine.domain.snack;
 
 public class Snack {
+    Integer id;
     String name;
-    int quantity;
-    int price;
+    Integer price;
     String description;
     String url;
 
-    public Snack(String name, int quantity, int price, String description, String url) {
+    public Snack(Integer id,String name, Integer price, String description, String url) {
+        this.id = id;
         this.name = name;
-        this.quantity = quantity;
         this.price = price;
         this.description = description;
         this.url = url;
     }
 
+    Integer getId(){
+        return id;
+    }
+    void setId(Integer id){
+        this.id = id;
+    }
 
+    String getName(){
+        return name;
+    }
+    void setName(String name){
+        this.name = name;
+    }
 
+    Integer getPrice(){
+        return price;
+    }
+    void setPrice(Integer price){
+        this.price = price;
+    }
+
+    String getDescription(){
+        return description;
+    }
+    void setDescription(){
+        this.description = description;
+    }
 }
